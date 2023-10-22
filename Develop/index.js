af4e3ce4be6fb6a1f0952065ed6a4b23ea4da9c8
Simon Hamblin <simon.hamblin@gmail.com>
2023-10-22 13:59:25 +1100

    array of questions created

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,50 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'contents',
+        message: 'Write a Table of Contents for your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write the installation information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write the unsage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Write the license information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Write the contributing information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write the tests information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Write the questions information for your project.',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
